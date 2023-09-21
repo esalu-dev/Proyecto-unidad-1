1e56bbd77bd0eb828206f01ff677d3a04d30f2a0
esalu-dev <esalu88@outlook.com>
2023-09-21 12:44:08 -0600

    Added palette

---
+++ src/main/java/com/example/constants/Colors.java
@@ -0,0 +1,11 @@
+package com.example.constants;
+
+import java.awt.Color;
+
+public class Colors {
+   public final static Color mainWhite = java.awt.Color.decode("#F2F1DC");
+   public final static Color mainYellow = java.awt.Color.decode("#F2C438");
+   public final static Color mostaza = java.awt.Color.decode("#D99B29");
+   public final static Color darkYellow = java.awt.Color.decode("#D98841");
+   public final static Color mainRed = java.awt.Color.decode("#BF3939");
+}

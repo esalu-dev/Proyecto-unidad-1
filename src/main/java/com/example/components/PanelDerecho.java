dd428a46f374e6b64f70e13e81f155f23035bd29
esalu-dev <esalu88@outlook.com>
2023-09-21 12:53:38 -0600

    Added right panel component

---
+++ src/main/java/com/example/components/PanelDerecho.java
@@ -0,0 +1,13 @@
+package com.example.components;
+
+import javax.swing.JPanel;
+
+import com.example.constants.Colors;
+
+public class PanelDerecho extends JPanel {
+   public PanelDerecho(){
+      setSize(300,720);
+      setBackground(Colors.mainBlue);
+   }
+   
+}

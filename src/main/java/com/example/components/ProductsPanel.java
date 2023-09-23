cf3086537f766d15555bd285595937d5ebf50c21
esalu-dev <esalu88@outlook.com>
2023-09-23 10:29:33 -0600

    Added getting data from local JSON

--- src/main/java/com/example/components/ProductsPanel.java
+++ src/main/java/com/example/components/ProductsPanel.java
@@ -5,22 +5,27 @@ import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
+import com.example.providers.JSONManager;
+import com.example.types.Mueble;
+
 
 public class ProductsPanel extends JPanel {
-   Card card = new Card("Facebook", "$1000.50", "facebook.png", true);
-   Card card2 = new Card("Google", "$2000.50", "google.png", false);
    final static GridLayout grid = new GridLayout(0, 2);
+
+   public void getData(){
+      for(Mueble mueble : JSONManager.getDataFromLocalJSON()){
+         add(new Card(mueble.getNombre(), String.valueOf(mueble.getPrecio()), mueble.getImgLink(), mueble.isAvailable()));
+      }
+
+   }
+
    public ProductsPanel(){
       setSize(1000, 500);
       setBackground(Color.WHITE);
       grid.setHgap(15); // Espacio horizontal entre celdas
       grid.setVgap(15);
       setLayout(grid);
-      add(card);
-      add(card2);
-      for(int i = 0; i < 10; i++){
-         add(new Card("Facebook", "$1000.50", "facebook.png", true));
-      }
+      getData();
       
    }
 }
